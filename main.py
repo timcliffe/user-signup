2afb5134e91f05d566051b73cff0a17f32402a24
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-01 14:23:19 -0500

    commit

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ app.config['DEBUG'] = True
 
 @app.route("/", methods=["POST"])
 def sign_up():
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
+    if username == "":
+        username_error = "invalid username"
+    if password = "":
+        password_error = "invalid password"
+    if verify_error = "":
+        verify_error = "invalid verification"
+    if email = "":
+        email_error = "invalid email"
 
 
 @app.route("/")
