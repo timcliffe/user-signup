4a44a03ee302ccf7732ffc44625e4dbed2d7346c
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-01 14:16:24 -0500

    testero

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import Flask, request, render_template
 app = Flask (__name__)
 app.config['DEBUG'] = True
 
+@app.route("/", methods=["POST"])
+def sign_up():
+
 
 @app.route("/")
 def index():
