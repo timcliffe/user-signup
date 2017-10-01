12d33e9f44153cbf750fe3324620078e15cb79e4
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-01 14:37:19 -0500

    Derp

--- main.py
+++ main.py
@@ -15,14 +15,23 @@ def sign_up():
     verify_error = ""
     email_error = ""
 
-    if username == "":
+    if username == "" or " " or len(username) < 3 or len(username) > 20:
         username_error = "invalid username"
-    if password = "":
+    if password = "" or " " or len(password) < 3 or len(password) > 20:
         password_error = "invalid password"
-    if verify_error = "":
+    if verify = "" or verify != password:
         verify_error = "invalid verification"
-    if email = "":
-        email_error = "invalid email"
+    if email != "":
+        if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 20:
+            email_error = "invalid email"
+
+
+    if email_error == "" and username_error == "" and verify_error == "" and password_error == "":
+        return render_template("welcome.html", username = username)
+    else:
+        return render_template("index.html", username_error = username_error,
+        password_error = password_error, verify_error = verify_error, username = username,
+        email = email)
 
 
 @app.route("/")
